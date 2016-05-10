2f3ba776c1f1304b4a3188ef6180c2fa39db232f
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-05-10 18:59:36 -0400

    started main handler

--- gh_portfolio/__init__.py
+++ gh_portfolio/__init__.py
@@ -1,1 +1,12 @@
 import flask
+import requests
+from os import getenv
+
+GITHUB_API_TOKEN = getenv("GITHUB_API_TOKEN")
+GITHUB_USERNAME  = getenv("GITHUB_USERNAME")
+
+@app.route("/")
+def index():
+    stats = get_gh_stats()
+    repos = get_repo_info()
+    return flask.render_template("index.html", {'repos': repos, 'stats': stats})
