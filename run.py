4dd8c94e0b8939f903f121d03d3348af5a57e20f
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-10-12 12:10:18 -0700

    fixed

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from gh_portfolio import app
 
-app.logger.info("Starting server")
+print("Starting server")
 http_server = HTTPServer(WSGIContainer(app))
 http_server.listen(8080)
-app.logger.info("Listening on port: 8080")
+print("Listening on port: 8080")
 IOLoop.instance().start()
